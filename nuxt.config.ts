9d1e4e29fd20aed59de0b0d9f77a7176881ec749
mathieuMarthy <mathieu.marthy@gmail.com>
2026-02-02 20:24:13 +0100

    remove nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
     css: ["./app/assets/css/main.css"],
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
     nitro: {
         preset: "bun",
     },
-    modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxtjs/i18n"],
+    modules: ["@nuxt/eslint", "@nuxtjs/i18n"],
     i18n: {
         defaultLocale: "en",
         locales: [
