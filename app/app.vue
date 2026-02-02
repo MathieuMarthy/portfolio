e1c55965a66c884fe0fdf78518b6259bd0c5bd69
mathieuMarthy <mathieu.marthy@gmail.com>
2026-02-03 00:22:34 +0100

    create the button to switch between light and dark mode

--- app/app.vue
+++ app/app.vue
@@ -1,39 +1,5 @@
-<script lang="ts" setup>
-import { onMounted, ref } from "vue";
-
-const isDark = ref(false);
-
-function toggleTheme() {
-    isDark.value = !isDark.value;
-    updateTheme();
-}
-
-function updateTheme() {
-    document.documentElement.classList.toggle("dark");
-    localStorage.setItem("theme", isDark.value ? "dark": "light");
-}
-
-onMounted(() => {
-    const stored = localStorage.getItem("theme");
-    if (stored) {
-        isDark.value = stored === "dark";
-    }
-    else {
-        isDark.value = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    }
-    updateTheme();
-});
-</script>
-
 <template>
-    <div class="bg-background h-screen">
-        <p class="text-4xl text-">{{ $t("hello") }}</p>
-
-        <button
-            class="bg-primary text-background px-4 py-2 rounded cursor-pointer hover:opacity-90"
-            @click="toggleTheme"
-        >
-            {{ isDark ? 'Light Mode' : 'Dark Mode' }}
-        </button>
+    <div class="bg-background h-screen transition duration-400">
+        <header-navbar/>
     </div>
 </template>
