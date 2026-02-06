a9e4f14eb4e6822e80523cfeabd28026322d4795
mathieuMarthy <mathieu.marthy@gmail.com>
2026-02-06 18:34:47 +0100

    add a view more button for projects

--- app/components/atoms/simple-button.vue
+++ app/components/atoms/simple-button.vue
@@ -1,15 +1,28 @@
 <script lang="ts" setup>
+import { ButtonTypes } from "~/composables/data/buttonTypes";
+
 defineProps({
     text: { type: String, required: true },
     clickHandler: { type: Function, required: false, default: null },
     styleClass: { type: String, required: false, default: "" },
     icon: { type: String, required: false, default: "" },
+    buttonType: { type: Number as PropType<ButtonTypes>, required: false, default: ButtonTypes.PRIMARY },
 });
+
+function getClassByType(type: ButtonTypes): string {
+    switch (type) {
+        case ButtonTypes.PRIMARY:
+            return "bg-primary text-text";
+        case ButtonTypes.SECONDARY:
+            return "bg-transparent text-text border-2 border-primary";
+    }
+}
+
 </script>
 
 <template>
     <button
-        :class="`${styleClass} bg-primary text-text font-semibold px-4 py-2 rounded-2xl cursor-pointer hover:bg-primary-hover
+        :class="`${styleClass} ${getClassByType(buttonType)} font-semibold px-4 py-2 rounded-2xl cursor-pointer hover:bg-primary-hover
             transition-colors duration-200 ease-in-out flex items-center justify-center gap-2`"
         @click="clickHandler && clickHandler()"
     >
--- app/components/organisms/personal-projects.vue
+++ app/components/organisms/personal-projects.vue
@@ -1,17 +1,32 @@
 ﻿<script lang="ts" setup>
 import { projects } from "~/composables/data/projects";
+import { ButtonTypes } from "~/composables/data/buttonTypes";
+
+const displayAllProjects = ref(false);
+
+const projectsToShow = computed(() => {
+    return displayAllProjects.value ? projects : projects.slice(0, 3);
+});
+
 </script>
 
 <template>
-    <section class="flex flex-col justify-center items-center gap-22">
+    <section class="flex flex-col justify-center items-center gap-22 w-full">
         <h2 class="text-text text-5xl text-center">{{ $t("my-projects.my-personals-projects") }}</h2>
 
-        <div class="flex flex-wrap justify-center gap-12 w-2/3">
+        <div class="flex flex-wrap justify-center gap-12 w-9/12">
             <molecules-project-card
-                v-for="project in projects"
+                v-for="project in projectsToShow"
                 :key="project.projectName"
                 :project="project"
             />
         </div>
+
+        <atoms-simple-button
+            v-if="!displayAllProjects"
+            :button-type="ButtonTypes.SECONDARY"
+            :click-handler="() => displayAllProjects = true"
+            text="view more"
+        />
     </section>
 </template>
