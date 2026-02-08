71b28abaf9e092c715aaf207b921c41cb2c1d9e9
mathieuMarthy <mathieu.marthy@gmail.com>
2026-02-08 18:17:50 +0100

    optimize left/right align

--- app/components/molecules/experience-bloc.vue
+++ app/components/molecules/experience-bloc.vue
@@ -2,29 +2,25 @@
 
 defineProps({
     experienceTitle: { type: String, required: true },
-    alignOnRight: { type: Boolean, required: true },
+    alignLeft: { type: Boolean, default: false },
 });
 </script>
 
 <template>
     <article class="flex flex-col gap-2 w-full md:w-1/2">
-        <div :class="alignOnRight ? 'pr-7': 'pl-7'">
+        <div :class="{ 'md:pl-0 md:pr-7': alignLeft }" class="pl-7">
             <p class="text-text-secondary text-sm">{{ $t(`my-experiences.${experienceTitle}-dates`) }}</p>
             <p class="text-text font-semibold">{{ $t(`my-experiences.${experienceTitle}-title`) }}</p>
         </div>
 
-        <div :class="'flex items-center gap-2 ' + (alignOnRight ? 'flex-row-reverse pr-0.5': 'pl-0.5')">
+        <div :class="{ 'md:flex-row-reverse md:pl-0 md:pr-0.5': alignLeft }" class="flex items-center gap-2 pl-0.5">
             <div class="size-4 bg-primary rounded-full"/>
             <div class="bg-gray-500 w-full h-0.5"/>
         </div>
 
-        <div :class="alignOnRight ? 'pr-2': 'pl-2'">
-            <p
-                :class="'text-text ' +
-                    (alignOnRight ?
-                        'border-r border-r-gray-500 pr-4':
-                        'border-l border-l-gray-500 pl-4')"
-            >{{ $t(`my-experiences.${experienceTitle}-description`) }}</p>
+        <div :class="{ 'md:pl-0 md:pr-2 md:flex-row-reverse': alignLeft }" class="flex gap-5 pl-2">
+            <div class="bg-gray-500 w-0.5 h-full scale-y-120 origin-top"/>
+            <p class="text-text w-full">{{ $t(`my-experiences.${experienceTitle}-description`) }}</p>
         </div>
     </article>
 </template>
--- app/components/organisms/my-experiences.vue
+++ app/components/organisms/my-experiences.vue
@@ -1,42 +1,20 @@
 <script lang="ts" setup>
 import { experiences } from "~/composables/data/experiences";
-
-const windowWidth = ref(0);
-const isLargeWindow = computed(() => {
-    return windowWidth.value > 768; // 768px = tailwind md breakpoint
-});
-
-function updateWindowWidth() {
-    windowWidth.value =  window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-}
-
-onMounted(() => {
-    updateWindowWidth();
-    window.addEventListener("resize", updateWindowWidth);
-});
-
-onUnmounted(() => {
-    window.removeEventListener("resize", updateWindowWidth);
-});
 </script>
 
 <template>
     <section class="flex flex-col justify-center items-center gap-22 w-full">
         <h2 class="text-text text-5xl text-center">{{ $t("my-experiences.my-experiences") }}</h2>
 
-        <div class="flex flex-col items-center gap-8 w-full px-24">
+        <div class="flex flex-col items-center gap-2 w-full px-24">
             <div
                 v-for="(experience, index) in experiences"
                 :key="experience"
-                :class="[
-                    'w-full flex',
-                    isLargeWindow ?
-                        (index % 2 == 1 ? 'justify-start pl-4' : 'justify-end pr-4'):
-                        'justify-center',
-                ]"
+                :class="index % 2 === 1 ? 'md:justify-start md:pl-4.5' : 'md:justify-end md:pr-4.5'"
+                class="w-full flex justify-center"
             >
                 <molecules-experience-bloc
-                    :align-on-right="isLargeWindow && index % 2 == 1"
+                    :align-left="index % 2 === 1"
                     :experience-title="experience"
                 />
             </div>
